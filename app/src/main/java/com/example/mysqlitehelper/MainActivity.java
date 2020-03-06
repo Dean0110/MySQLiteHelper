322662efdb5c66a42a983fba5b15420b4c5c4db2
Dean0110 <wang904183923@163.com>
2020-03-06 21:15:08 +0800

    Layout update
    Makes layout more comfortable.

--- app/src/main/java/com/example/mysqlitehelper/MainActivity.java
+++ app/src/main/java/com/example/mysqlitehelper/MainActivity.java
@@ -23,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edt_name.findViewById(R.id.edt_name);
-        edt_email.findViewById(R.id.edt_email);
-        edt_phone.findViewById(R.id.edt_phone);
-        btn_write.findViewById(R.id.btn_write);
-        btn_read.findViewById(R.id.btn_read);
-        btn_update.findViewById(R.id.btn_update);
-        btn_remove.findViewById(R.id.btn_remove);
+        edt_name=findViewById(R.id.edt_name);
+        edt_email=findViewById(R.id.edt_email);
+        edt_phone=findViewById(R.id.edt_phone);
+        btn_write=findViewById(R.id.btn_write);
+        btn_read=findViewById(R.id.btn_read);
+        btn_update=findViewById(R.id.btn_update);
+        btn_remove=findViewById(R.id.btn_remove);
         mySQL=new MySQLite(this,"mydb",null,1);
     }
 
@@ -47,18 +47,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void BTNWrite(View view) {
+    public void btnWrite(View view) {
         Insert("name",edt_name.getText().toString());
         Insert("email",edt_email.getText().toString());
         Insert("phone",edt_phone.getText().toString());
     }
 
-    public void BTNRead(View view) {
+    public void btnRead(View view) {
     }
 
-    public void BTNUpdate(View view) {
+    public void btnUpdate(View view) {
     }
 
-    public void BTNRemove(View view) {
+    public void btnRemove(View view) {
     }
 }
